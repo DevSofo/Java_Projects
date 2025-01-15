9350105ca600d402dc9f75f604e13ccf71e5afe7
Sofoklis <sofoklismaniatopoulos@gmail.com>
2025-01-15 23:58:28 +0200

    Edit User using PUT Mapping

--- fullstack_backend/src/main/java/com/devsofo/fullstack_backend/controller/UserController.java
+++ fullstack_backend/src/main/java/com/devsofo/fullstack_backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.devsofo.fullstack_backend.controller;
 
+import com.devsofo.fullstack_backend.exception.UserNotFoundException;
 import com.devsofo.fullstack_backend.model.User;
 import com.devsofo.fullstack_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,23 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                })
+                .orElseThrow(() -> new UserNotFoundException(id));
     }
 
 
