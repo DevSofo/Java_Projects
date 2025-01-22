050bbbad581a58b2427a0be3a0b4d956f775384e
Sofoklis <sofoklismaniatopoulos@gmail.com>
2025-01-22 02:20:28 +0200

    End of the backend development

--- SRBApplication/src/main/java/com/devsofo/srbapplication/repository/StudentRepository.java
+++ SRBApplication/src/main/java/com/devsofo/srbapplication/repository/StudentRepository.java
@@ -2,9 +2,10 @@ package com.devsofo.srbapplication.repository;
 
 import com.devsofo.srbapplication.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findByEmail(String email);
 }
--- SRBApplication/src/main/java/com/devsofo/srbapplication/service/IStudentService.java
+++ SRBApplication/src/main/java/com/devsofo/srbapplication/service/IStudentService.java
@@ -5,13 +5,10 @@ import com.devsofo.srbapplication.model.Student;
 import java.util.List;
 
 public interface IStudentService {
-
     Student addStudent(Student student);
     List<Student> getStudents();
-    Student updateStudent(Student student);
-
     Student updateStudent(Student student, Long id);
-
     Student getStudentById(Long id);
-    void  deleteStudent(Long id);
+    void deleteStudent(Long id);
 }
+
