8e157cf8e8bbf7afb21d07f75eb06ee09f94db8f
Sofoklis <sofoklismaniatopoulos@gmail.com>
2025-01-23 14:41:13 +0200

    Added the NavBar in SRB App

--- sbr_client/src/App.js
+++ sbr_client/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import StudentView from './component/student/StudentsView';
-import Home from './Home';
+import Home from "./Home";
+import StudentsView from './component/student/StudentsView';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
+import NavBar from './component/common/NavBar';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to our front end</h2>
-      <Home/>
-      <StudentView/>
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route exact path='/' element={<Home/>} ></Route>
+          <Route exact path='/view-students' element={<StudentsView/>} ></Route>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
