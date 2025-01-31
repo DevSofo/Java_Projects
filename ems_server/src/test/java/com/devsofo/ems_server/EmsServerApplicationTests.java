4e39199dba881253f5f792158e6f297cb5f9709c
Sofoklis <sofoklismaniatopoulos@gmail.com>
2025-01-31 14:36:37 +0200

    Started the EMS project, created the Employee Entity

---
+++ ems_server/src/test/java/com/devsofo/ems_server/EmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsofo.ems_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
