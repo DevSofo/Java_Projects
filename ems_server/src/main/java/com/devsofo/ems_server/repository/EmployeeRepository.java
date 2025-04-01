17f0ed3df0f3cfb8db4bf1512293bb513a6f1020
Sofoklis <sofoklismaniatopoulos@gmail.com>
2025-04-01 16:22:41 +0300

    Deleted all the EMS PROJECT

--- ems_server/src/main/java/com/devsofo/ems_server/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devsofo.ems_server.repository;
-
-import com.devsofo.ems_server.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
