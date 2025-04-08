635ace7372206fd1b974ff6363a3bd9f36ef61f6
Sofoklis <sofoklismaniatopoulos@gmail.com>
2025-04-08 17:06:45 +0300

    Created the Header, Footer of the App

--- Employee Management System/ems-frontend/src/App.jsx
+++ Employee Management System/ems-frontend/src/App.jsx
@@ -1,12 +1,18 @@
-
 import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
+import ListEmployeeCompontent from './components/ListEmployeeCompontent'
+
+
 
 function App() {
   
 
   return (
     <>
-      
+      <HeaderComponent/>
+      <ListEmployeeCompontent/>
+      <FooterComponent/>
     </>
   )
 }
