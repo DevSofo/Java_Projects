e4bb113b58a30d68ba6f6c7270fa06a8d8446aad
Sofoklis <sofoklismaniatopoulos@gmail.com>
2025-04-09 14:46:41 +0300

    Until 2.36.19

--- Employee Management System/ems-frontend/src/App.jsx
+++ Employee Management System/ems-frontend/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeCompontent from './components/ListEmployeeCompontent'
+import EmployeeComponent from './components/EmployeeComponent'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 
 
@@ -10,9 +12,18 @@ function App() {
 
   return (
     <>
-      <HeaderComponent/>
-      <ListEmployeeCompontent/>
-      <FooterComponent/>
+      <BrowserRouter>
+        <HeaderComponent/>
+          <Routes>
+            {/* //http://localhost:3000 */}
+            <Route path='/' element={<ListEmployeeCompontent/>}></Route>
+            {/* //http://localhost:3000/employees */}
+            <Route path='/employees' element={<ListEmployeeCompontent/>}></Route>
+            {/* //http://localhost:3000/add-employee */}
+            <Route path='/add-employee' element={<EmployeeComponent/>}></Route>
+          </Routes>
+        <FooterComponent/>
+      </BrowserRouter>
     </>
   )
 }
